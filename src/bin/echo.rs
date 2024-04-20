0730bf7867e843e853d04f1db7faa8e0972acb48
Vivitsu Maharaja <vivitsu.maharaja@gmail.com>
2024-04-20 09:38:04 -0700

    Refactor

---
+++ src/bin/echo.rs
@@ -0,0 +1,48 @@
+use mjollnir::*;
+
+use anyhow::Context;
+use serde::{Deserialize, Serialize};
+use std::io::{StdoutLock, Write};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+enum Payload {
+    Echo { echo: String },
+    EchoOk { echo: String },
+}
+
+struct EchoNode {
+    id: usize,
+}
+
+impl Node<(), Payload> for EchoNode {
+    fn from_init(_state: (), _init: Init) -> anyhow::Result<Self> {
+        Ok(EchoNode { id: 0 })
+    }
+
+    fn step(&mut self, input: Message<Payload>, output: &mut StdoutLock) -> anyhow::Result<()> {
+        match input.body.payload {
+            Payload::Echo { echo } => {
+                let reply = Message {
+                    src: input.dst,
+                    dst: input.src,
+                    body: Body {
+                        id: Some(self.id),
+                        in_reply_to: input.body.id,
+                        payload: Payload::EchoOk { echo },
+                    },
+                };
+                serde_json::to_writer(&mut *output, &reply).context("respond to init")?;
+                output.write_all(b"\n").context("write trailing newline")?;
+                self.id += 1;
+            }
+            Payload::EchoOk { .. } => {}
+        }
+        Ok(())
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    run::<_, EchoNode, _>(())
+}
---
+++ src/lib.rs
@@ -0,0 +1,89 @@
+use anyhow::Context;
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use std::io::{BufRead, StdoutLock, Write};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Message<Payload> {
+    pub src: String,
+    #[serde(rename = "dest")]
+    pub dst: String,
+    pub body: Body<Payload>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Body<Payload> {
+    #[serde(rename = "msg_id")]
+    pub id: Option<usize>,
+    pub in_reply_to: Option<usize>,
+    #[serde(flatten)]
+    pub payload: Payload,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Init {
+    pub node_id: String,
+    pub node_ids: Vec<String>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+enum InitPayload {
+    Init(Init),
+    InitOk,
+}
+
+pub trait Node<S, Payload> {
+    fn from_init(state: S, init: Init) -> anyhow::Result<Self>
+    where
+        Self: Sized;
+
+    fn step(&mut self, input: Message<Payload>, output: &mut StdoutLock) -> anyhow::Result<()>;
+}
+
+pub fn run<S, N, P>(init_state: S) -> anyhow::Result<()>
+where
+    P: DeserializeOwned,
+    N: Node<S, P>,
+{
+    let stdin = std::io::stdin().lock();
+    let mut stdin = stdin.lines();
+    let mut stdout = std::io::stdout().lock();
+
+    let init_msg: Message<InitPayload> = serde_json::from_str(
+        &stdin
+            .next()
+            .expect("no init message")
+            .context("failed to read init message from stdin")?,
+    )
+    .context("could not deserialize init message")?;
+
+    let InitPayload::Init(init) = init_msg.body.payload else {
+        panic!("first message should be init");
+    };
+
+    let mut node: N = Node::from_init(init_state, init).context("node initializing failed")?;
+
+    let reply = Message {
+        src: init_msg.dst,
+        dst: init_msg.src,
+        body: Body {
+            id: Some(0),
+            in_reply_to: init_msg.body.id,
+            payload: InitPayload::InitOk,
+        },
+    };
+
+    serde_json::to_writer(&mut stdout, &reply).context("serialize response to init message")?;
+    stdout.write_all(b"\n").context("write trailing newline")?;
+
+    for line in stdin {
+        let line = line.context("Could not read STDIN from maelstrom")?;
+        let input = serde_json::from_str(&line)
+            .context("Maelstrom input from STDIN could not be deserialized")?;
+        node.step(input, &mut stdout)
+            .context("Node step function failed")?;
+    }
+
+    Ok(())
+}
